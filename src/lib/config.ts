22cae0037787a651c1b7c81f77cd5181acc879ec
Soomin Choi <spacegaori@d142-058-209-156.wireless.sfu.ca>
2023-10-20 20:34:28 -0700

    set proper url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from "$app/environment"
 
 export const title = "Mukiman's Den"
 export const description = 'Den for those...'
-export const url = dev ? 'http://localhost:5173/' : 'asdf'
+export const url = dev ? 'http://localhost:5173/' : 'https://mukimans-den.vercel.app/'
